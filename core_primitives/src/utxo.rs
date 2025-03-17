7f512474dc0887b2bf3064a0a16aee2fc8ef0d27
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2025-03-17 13:18:44 +0200

    fix: adding core primitives crate

---
+++ core_primitives/src/utxo.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use log::info;
+use serde::{Deserialize, Serialize};
+use sha2::{digest::FixedOutput, Digest};
+use storage::{merkle_tree_public::TreeHashType, nullifier::UTXONullifier, AccountId};
+
+///Raw asset data
+pub type Asset = Vec<u8>;
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+///Container for raw utxo payload
+pub struct UTXO {
+    pub hash: TreeHashType,
+    pub owner: AccountId,
+    pub nullifier: Option<UTXONullifier>,
+    pub asset: Asset,
+    // TODO: change to u256
+    pub amount: u128,
+    pub privacy_flag: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UTXOPayload {
+    pub owner: AccountId,
+    pub asset: Asset,
+    // TODO: change to u256
+    pub amount: u128,
+    pub privacy_flag: bool,
+}
