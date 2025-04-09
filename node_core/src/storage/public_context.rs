6447545ec4ea3c0dd6f00041e8ad64c375bd2944
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2025-04-09 01:32:16 -0400

    serialiaze `PublicSCContext`

--- node_core/src/storage/public_context.rs
+++ node_core/src/storage/public_context.rs
@@ -1,9 +1,11 @@
 use std::collections::BTreeMap;
 
 use accounts::account_core::{AccountAddress, AccountPublicMask};
+use serde::{Serialize};
 use storage::merkle_tree_public::TreeHashType;
 
 ///Strucutre, representing context, given to a smart contract on a call
+#[derive(Serialize)]
 pub struct PublicSCContext {
     pub caller_address: AccountAddress,
     pub caller_balance: u64,
