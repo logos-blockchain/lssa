6565e0af184da969dbb62b8abe98a998b7e5c5a0
Oleksandr Pravdyvyi <awxrvtb@gmail.com>
2025-08-04 15:09:28 +0300

    breaking: initial cuts

--- node_core/src/chain_storage/mod.rs
+++ node_core/src/chain_storage/mod.rs
@@ -2,24 +2,18 @@ use std::collections::{BTreeMap, HashMap, HashSet};
 
 use accounts::account_core::{address::AccountAddress, Account};
 use anyhow::Result;
-use block_store::NodeBlockStore;
 use common::{
     block::Block,
-    execution_input::PublicNativeTokenSend,
     merkle_tree_public::merkle_tree::{PublicTransactionMerkleTree, UTXOCommitmentsMerkleTree},
     nullifier::UTXONullifier,
-    utxo_commitment::UTXOCommitment,
 };
-use k256::AffinePoint;
-use log::{info, warn};
 use sc_core::public_context::PublicSCContext;
 use serde::{Deserialize, Serialize};
-use utxo::utxo_core::UTXO;
 
-use crate::{config::NodeConfig, ActionData};
+use crate::config::NodeConfig;
 
 pub mod accounts_store;
-pub mod block_store;
+//pub mod block_store;
 
 #[derive(Deserialize, Serialize)]
 pub struct AccMap {
@@ -49,7 +43,6 @@ impl From<AccMap> for HashMap<[u8; 32], Account> {
 
 pub struct NodeChainStore {
     pub acc_map: HashMap<AccountAddress, Account>,
-    pub block_store: NodeBlockStore,
     pub nullifier_store: HashSet<UTXONullifier>,
     pub utxo_commitments_store: UTXOCommitmentsMerkleTree,
     pub pub_tx_store: PublicTransactionMerkleTree,
@@ -58,30 +51,15 @@ pub struct NodeChainStore {
 
 impl NodeChainStore {
     pub fn new(config: NodeConfig, genesis_block: Block) -> Result<(Self, u64)> {
-        let mut acc_map = HashMap::new();
-        let mut nullifier_store = HashSet::new();
-        let mut utxo_commitments_store = UTXOCommitmentsMerkleTree::new(vec![]);
-        let mut pub_tx_store = PublicTransactionMerkleTree::new(vec![]);
-        let mut block_id = genesis_block.block_id;
-
-        //Sequencer should panic if unable to open db,
-        //as fixing this issue may require actions non-native to program scope
-        let block_store =
-            NodeBlockStore::open_db_with_genesis(&config.home.join("rocksdb"), Some(genesis_block))
-                .unwrap();
-
-        if let Ok(temp_block_id) = block_store.get_snapshot_block_id() {
-            utxo_commitments_store = block_store.get_snapshot_commitment()?;
-            nullifier_store = block_store.get_snapshot_nullifier()?;
-            acc_map = block_store.get_snapshot_account()?;
-            pub_tx_store = block_store.get_snapshot_transaction()?;
-            block_id = temp_block_id;
-        }
+        let acc_map = HashMap::new();
+        let nullifier_store = HashSet::new();
+        let utxo_commitments_store = UTXOCommitmentsMerkleTree::new(vec![]);
+        let pub_tx_store = PublicTransactionMerkleTree::new(vec![]);
+        let block_id = genesis_block.block_id;
 
         Ok((
             Self {
                 acc_map,
-                block_store,
                 nullifier_store,
                 utxo_commitments_store,
                 pub_tx_store,
@@ -89,184 +67,6 @@ impl NodeChainStore {
             },
             block_id,
         ))
-    }
-
-    pub fn new_after_restart(config: NodeConfig, genesis_block: Block) -> Result<(Self, u64)> {
-        let mut acc_map = HashMap::new();
-        let mut nullifier_store = HashSet::new();
-        let mut utxo_commitments_store = UTXOCommitmentsMerkleTree::new(vec![]);
-        let mut pub_tx_store = PublicTransactionMerkleTree::new(vec![]);
-        let mut block_id = genesis_block.block_id;
-
-        //Sequencer should panic if unable to open db,
-        //as fixing this issue may require actions non-native to program scope
-        let block_store = NodeBlockStore::open_db_reload(&config.home.join("rocksdb")).unwrap();
-
-        if let Ok(temp_block_id) = block_store.get_snapshot_block_id() {
-            utxo_commitments_store = block_store.get_snapshot_commitment()?;
-            nullifier_store = block_store.get_snapshot_nullifier()?;
-            acc_map = block_store.get_snapshot_account()?;
-            pub_tx_store = block_store.get_snapshot_transaction()?;
-            block_id = temp_block_id;
-        }
-
-        Ok((
-            Self {
-                acc_map,
-                block_store,
-                nullifier_store,
-                utxo_commitments_store,
-                pub_tx_store,
-                node_config: config,
-            },
-            block_id,
-        ))
-    }
-
-    pub fn dissect_insert_block(&mut self, block: Block) -> Result<()> {
-        let block_id = block.block_id;
-
-        for tx in &block.transactions {
-            if !tx.body().execution_input.is_empty() {
-                let public_action =
-                    serde_json::from_slice::<ActionData>(&tx.body().execution_input);
-
-                if let Ok(public_action) = public_action {
-                    match public_action {
-                        ActionData::MintMoneyPublicTx(action) => {
-                            let acc_mut = self.acc_map.get_mut(&action.acc);
-
-                            if let Some(acc_mut) = acc_mut {
-                                acc_mut.balance += action.amount as u64;
-                            }
-                        }
-                        ActionData::SendMoneyDeshieldedTx(action) => {
-                            for (balance, acc_addr) in action.receiver_data {
-                                let acc_mut = self.acc_map.get_mut(&acc_addr);
-
-                                if let Some(acc_mut) = acc_mut {
-                                    acc_mut.balance += balance as u64;
-                                }
-                            }
-                        }
-                        ActionData::SendMoneyShieldedTx(action) => {
-                            let acc_mut = self.acc_map.get_mut(&action.acc_sender);
-
-                            if let Some(acc_mut) = acc_mut {
-                                acc_mut.balance =
-                                    acc_mut.balance.saturating_sub(action.amount as u64);
-                            }
-                        }
-                        _ => {}
-                    }
-                } else {
-                    let native_transfer =
-                        serde_json::from_slice::<PublicNativeTokenSend>(&tx.body().execution_input);
-
-                    if let Ok(transfer) = native_transfer {
-                        if let Some(acc_sender) = self.acc_map.get_mut(&transfer.from) {
-                            //Can panic, we depend on sequencer maintaining chain consistency here
-                            acc_sender.balance -= transfer.balance_to_move;
-
-                            if let Some(acc_rec) = self.acc_map.get_mut(&transfer.to) {
-                                acc_rec.balance += transfer.balance_to_move;
-                            }
-                        }
-                    }
-                }
-            }
-
-            self.utxo_commitments_store.add_tx_multiple(
-                tx.body()
-                    .utxo_commitments_created_hashes
-                    .clone()
-                    .into_iter()
-                    .map(|hash| UTXOCommitment { hash })
-                    .collect(),
-            );
-
-            for nullifier in tx.body().nullifier_created_hashes.iter() {
-                self.nullifier_store.insert(UTXONullifier {
-                    utxo_hash: *nullifier,
-                });
-            }
-
-            if !tx.body().encoded_data.is_empty() {
-                let ephemeral_public_key_sender =
-                    serde_json::from_slice::<AffinePoint>(&tx.body().ephemeral_pub_key)?;
-
-                for (ciphertext, nonce, tag) in tx.body().encoded_data.clone() {
-                    let slice = nonce.as_slice();
-                    let nonce =
-                        accounts::key_management::constants_types::Nonce::clone_from_slice(slice);
-                    for (acc_id, acc) in self.acc_map.iter_mut() {
-                        if hex::decode(acc_id).unwrap()[0] == tag {
-                            let decoded_data_curr_acc = acc.decrypt_data(
-                                ephemeral_public_key_sender,
-                                ciphertext.clone(),
-                                nonce,
-                            );
-                            if let Ok(decoded_data_curr_acc) = decoded_data_curr_acc {
-                                let decoded_utxo_try =
-                                    serde_json::from_slice::<UTXO>(&decoded_data_curr_acc);
-                                if let Ok(utxo) = decoded_utxo_try {
-                                    if &utxo.owner == acc_id {
-                                        acc.utxos.insert(utxo.hash, utxo);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            self.pub_tx_store.add_tx(tx);
-        }
-
-        self.block_store.put_block_at_id(block)?;
-
-        //Snapshot
-        if block_id.is_multiple_of(self.node_config.shapshot_frequency_in_blocks) {
-            //Serializing all important data structures
-
-            //If we fail serialization, it is not the reason to stop running
-            //Logging on warn level in this cases
-            let acc_map: AccMap = self.acc_map.clone().into();
-
-            if let Ok(accounts_ser) = serde_json::to_vec(&acc_map).inspect_err(|err| {
-                warn!("Failed to serialize accounts data {err:#?}");
-            }) {
-                if let Ok(comm_ser) =
-                    serde_json::to_vec(&self.utxo_commitments_store).inspect_err(|err| {
-                        warn!("Failed to serialize commitments {err:#?}");
-                    })
-                {
-                    if let Ok(txs_ser) = serde_json::to_vec(&self.pub_tx_store).inspect_err(|err| {
-                        warn!("Failed to serialize transactions {err:#?}");
-                    }) {
-                        if let Ok(nullifiers_ser) = serde_json::to_vec(&self.nullifier_store)
-                            .inspect_err(|err| {
-                                warn!("Failed to serialize nullifiers {err:#?}");
-                            })
-                        {
-                            let snapshot_trace = self.block_store.put_snapshot_at_block_id(
-                                block_id,
-                                accounts_ser,
-                                comm_ser,
-                                txs_ser,
-                                nullifiers_ser,
-                            );
-
-                            info!(
-                                "Snapshot executed at {block_id:?} with results {snapshot_trace:#?}"
-                            );
-                        }
-                    }
-                }
-            }
-        }
-
-        Ok(())
     }
 
     pub fn produce_context(&self, caller: AccountAddress) -> PublicSCContext {
@@ -298,7 +98,6 @@ mod tests {
     use crate::config::GasConfig;
     use accounts::account_core::Account;
     use common::block::{Block, Data};
-    use common::merkle_tree_public::TreeHashType;
     use common::transaction::{SignaturePrivateKey, Transaction, TransactionBody, TxKind};
     use secp256k1_zkp::Tweak;
     use std::path::PathBuf;
@@ -489,6 +288,7 @@ mod tests {
         }
     }
 
+    //ToDo: Continue refactor
     fn create_dummy_transaction(
         nullifier_created_hashes: Vec<[u8; 32]>,
         utxo_commitments_spent_hashes: Vec<[u8; 32]>,
@@ -515,6 +315,7 @@ mod tests {
         Transaction::new(body, SignaturePrivateKey::random(&mut rng))
     }
 
+    //ToDo: Continue refactor
     fn create_sample_block(block_id: u64, prev_block_id: u64) -> Block {
         Block {
             block_id,
@@ -551,10 +352,6 @@ mod tests {
         }
     }
 
-    fn generate_dummy_utxo(address: TreeHashType, amount: u128) -> UTXO {
-        UTXO::new(address, vec![], amount, false)
-    }
-
     #[test]
     fn test_new_initializes_correctly() {
         let temp_dir = tempdir().unwrap();
@@ -573,60 +370,5 @@ mod tests {
             store.utxo_commitments_store.get_root().unwrap_or([0; 32]),
             [0; 32]
         );
-    }
-
-    #[test]
-    fn test_new_recovers_from_snapshot() {
-        let temp_dir = tempdir().unwrap();
-        let path = temp_dir.path().to_path_buf();
-
-        let config = create_sample_node_config(path);
-
-        let nullifier_secret_const =
-            "261d61d294ac4bdc24f91b6f490efa263757a4a95f65871cd4f16b2ea23c3b5d";
-        std::env::set_var("NULLIFIER_SECRET_CONST", nullifier_secret_const);
-
-        let viewing_secret_const =
-            "6117af750b30d7a296672ec3b3b25d3489beca3cfe5770fa39f275cec395d5ce";
-        std::env::set_var("VIEWING_SECRET_CONST", viewing_secret_const);
-
-        let genesis_block = create_genesis_block();
-
-        // Initialize once to create DB and store fake snapshot
-        {
-            let (mut store, _) =
-                NodeChainStore::new(config.clone(), genesis_block.clone()).unwrap();
-
-            // Insert state
-            let mut account = Account::new();
-            account
-                .add_new_utxo_outputs(vec![generate_dummy_utxo(account.address, 100)])
-                .unwrap();
-            store.acc_map.insert(account.address, account);
-            store.nullifier_store.insert(UTXONullifier {
-                utxo_hash: [2u8; 32],
-            });
-            store
-                .utxo_commitments_store
-                .add_tx_multiple(vec![UTXOCommitment { hash: [3u8; 32] }]);
-            store.pub_tx_store.add_tx(&create_dummy_transaction(
-                vec![[9; 32]],
-                vec![[7; 32]],
-                vec![[8; 32]],
-            ));
-
-            // Put block snapshot to trigger snapshot recovery on next load
-            let dummy_block = create_sample_block(1, 0);
-
-            store.dissect_insert_block(dummy_block).unwrap();
-        }
-
-        // Now reload and verify snapshot is used
-        let (recovered_store, block_id) =
-            NodeChainStore::new_after_restart(config.clone(), genesis_block).unwrap();
-
-        assert_eq!(block_id, 1);
-        assert_eq!(recovered_store.acc_map.len(), 1);
-        assert!(recovered_store.utxo_commitments_store.get_root().is_some());
     }
 }
