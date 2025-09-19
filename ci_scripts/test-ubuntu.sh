f5f5ab4ef190ff11419d63452b41ab7d707b4238
Sergio Chouhy <sergio.chouhy@gmail.com>
2025-09-19 12:39:19 -0300

    add token program tests to ci tests

--- ci_scripts/test-ubuntu.sh
+++ ci_scripts/test-ubuntu.sh
@@ -5,7 +5,11 @@ curl -L https://risczero.com/install | bash
 source env.sh
 
 RISC0_DEV_MODE=1 cargo test --release
+
 cd integration_tests
 export NSSA_WALLET_HOME_DIR=$(pwd)/configs/debug/wallet/
 export RUST_LOG=info
 cargo run $(pwd)/configs/debug all
+cd ..
+
+cd nssa/program_methods/guest && cargo test --release
