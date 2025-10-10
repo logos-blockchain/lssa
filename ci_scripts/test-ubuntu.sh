e63336189f0488f94817b8e742fcb7996de5ebc4
Sergio Chouhy <sergio.chouhy@gmail.com>
2025-10-10 17:51:22 -0300

    fix integration tests

--- ci_scripts/test-ubuntu.sh
+++ ci_scripts/test-ubuntu.sh
@@ -4,16 +4,16 @@ curl -L https://risczero.com/install | bash
 /home/runner/.risc0/bin/rzup install 
 source env.sh
 
-RISC0_DEV_MODE=1 cargo test --release --features no_docker
+RISC0_DEV_MODE=1 cargo test --release --features no-docker
 
 cd integration_tests
 export NSSA_WALLET_HOME_DIR=$(pwd)/configs/debug/wallet/
 export RUST_LOG=info
-cargo run --features no_docker $(pwd)/configs/debug all
+cargo run $(pwd)/configs/debug all
 echo "Try test valid proof at least once"
-cargo run --features no_docker $(pwd)/configs/debug test_success_private_transfer_to_another_owned_account
+cargo run $(pwd)/configs/debug test_success_private_transfer_to_another_owned_account
 echo "Continuing in dev mode"
-RISC0_DEV_MODE=1 cargo run --features no_docker $(pwd)/configs/debug all
+RISC0_DEV_MODE=1 cargo run $(pwd)/configs/debug all
 cd ..
 
-cd nssa/program_methods/guest && cargo test --release --features no_docker
+cd nssa/program_methods/guest && cargo test --release
