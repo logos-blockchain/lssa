081a366c16824542a0883c248edb30768e438e2c
Daniil Polyakov <arjentix@gmail.com>
2025-12-16 19:03:27 +0300

    feat: add programs check in ci

--- ci_scripts/lint-ubuntu.sh
+++ ci_scripts/lint-ubuntu.sh
@@ -6,3 +6,5 @@ cargo install taplo-cli --locked
 taplo fmt --check
 
 RISC0_SKIP_BUILD=1 cargo clippy --workspace --all-targets -- -D warnings
+# Lint all programs individually to avoid feature unification issues
+RISC0_SKIP_BUILD=1 cargo clippy -p "*programs" -- -D warnings
