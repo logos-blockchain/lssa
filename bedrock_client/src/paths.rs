f817279d33d818fcdd937aedf37e365ce0b38283
Pravdyvy <awxrvtb@gmail.com>
2026-01-08 09:10:00 +0200

    fix: client as dependency

--- bedrock_client/src/paths.rs
+++
@@ -1,6 +0,0 @@
-pub const CRYPTARCHIA_INFO: &str = "/cryptarchia/info";
-pub const CRYPTARCHIA_LIB_STREAM: &str = "/cryptarchia/lib-stream";
-pub const STORAGE_BLOCK: &str = "/storage/block";
-
-pub const BLOCKS: &str = "/cryptarchia/blocks";
-pub const BLOCKS_STREAM: &str = "/cryptarchia/blocks/stream";
