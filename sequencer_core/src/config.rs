3dbe31501ce11682cad7223bc6c7f47307d2f11c
Sergio Chouhy <sergio.chouhy@gmail.com>
2026-01-14 23:45:58 -0300

    fix merge

--- sequencer_core/src/config.rs
+++ sequencer_core/src/config.rs
@@ -67,12 +67,3 @@ impl SequencerConfig {
         Ok(serde_json::from_reader(reader)?)
     }
 }
-
-impl SequencerConfig {
-    pub fn from_path(config_home: &Path) -> Result<SequencerConfig> {
-        let file = File::open(config_home)?;
-        let reader = BufReader::new(file);
-
-        Ok(serde_json::from_reader(reader)?)
-    }
-}
